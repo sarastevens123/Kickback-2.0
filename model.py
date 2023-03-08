463d4e9954eb1d4d7a4cd26d37e21216c4a0da66
Sara Stevens <sarastevens123@gmail.com>
2023-03-07 16:31:41 -0800

    initial commit

---
+++ model.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Guest(db.model):
+
+    __tablename__ = "guests"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    fname = db.Column(db.String(50), nullable=False)
+    lname = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    
+
+class Restaurant(db.model):
+
+    __tablename__ = "restaurants"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    
+
+class GuestReview(db.model):
+    """a review of a particular guest made by the restaurant"""
+
+    __tablename__ = "guest_reviews"
+
+class RestaurantReview(db.model):
+    """a review of a particular restaurant made by the guest that dined there"""
+
+    __tablename__ = "restaurant_reviews"
+
+class BannedGuest(db.model):
+    """an account of a guest that has been banned from a restaurant"""
+
+    __tablename__ = "banned_guests"
+
+    
