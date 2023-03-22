5e5ec65d99e115dd2bff8b66223f7c73445214f8
Sara Stevens <sarastevens123@gmail.com>
2023-03-21 18:17:41 -0700

    working_branch created

--- model.py
+++ model.py
@@ -2,25 +2,25 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class Guest(db.model):
+class GuestUser(db.model):
 
     __tablename__ = "guests"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    fname = db.Column(db.String(50), nullable=False)
-    lname = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    fname = db.Column(db.String(100), nullable=False)
+    lname = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     
 
-class Restaurant(db.model):
+class RestaurantUser(db.model):
 
     __tablename__ = "restaurants"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     
 
@@ -38,5 +38,9 @@ class BannedGuest(db.model):
     """an account of a guest that has been banned from a restaurant"""
 
     __tablename__ = "banned_guests"
+
+
+
+
 
     
