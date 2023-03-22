971ea984364d5bd3d5c1ce064d2f283938905f0f
Sara Stevens <sarastevens123@gmail.com>
2023-03-21 18:24:31 -0700

    added base.html file

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
+import 
 import json
 import model 
-from jijnja import StrictUndefined
 from flask import Flask, render_template,jsonify,request,session,flash,send_from_directory
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "dev"
 
-#Jinja templating debugging assist
-app.jinja_env.undefined = StrictUndefined
