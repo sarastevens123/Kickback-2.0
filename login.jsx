fe3857b28fa23db96fe24a645ce7935fe62d7bf1
Sara Stevens <sarastevens123@gmail.com>
2023-03-31 17:06:19 -0700

    updated index to include javascript libraries for react , and bootstrap. added static and css folders

---
+++ login.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        fetch()
+    }
+}
